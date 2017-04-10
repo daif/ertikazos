0f297abdd667a547e7df77d2ac35a74c814412aa
Daif Alotaibi <daif@daif.net>
2017-04-10 13:42:02 +0300

    Change migration_version Upgrade font awesom to 4.7

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20160901000001;
+$config['migration_version'] = 20160901000000;
 
 /*
 |--------------------------------------------------------------------------
